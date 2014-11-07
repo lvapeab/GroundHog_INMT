c158dea27ec9b82fb02db711f00983b52139277f
orhanf <orhan.firat@ceng.metu.edu.tr>
2014-11-07 00:27:39 -0500

    Print state script added

---
+++ scripts/printState.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cPickle
+import sys
+import pprint
+
+if __name__ == '__main__':
+    state_filename = sys.argv[1]
+    
+    state = {}
+    with open(state_filename) as src:
+        state.update(cPickle.load(src))
+
+    pprint.pprint(state)

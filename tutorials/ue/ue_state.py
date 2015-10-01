f365e8133c9298e84f0997435dc231cc60486dee
lvapeab <lvapeab@gmail.com>
2015-10-01 18:45:32 +0200

    Added some example state

---
+++ tutorials/ue/ue_state.py
@@ -0,0 +1,20 @@
+dict(
+path = "/home/lvapeab/smt/software/GroundHog/tutorials/DATA/miniue/miniue.npz",
+dictionary = "/home/lvapeab/smt/software/GroundHog/tutorials/DATA/miniue/miniue_dict.npz",
+chunks = 'words',
+n_in = 10001,
+n_out = 10001,
+
+trainFreq = 500,
+hookFreq = 1000,
+validFreq = 1000,
+
+
+inp_nhids = '[400]',
+dout_nhid = '400',
+nhids = '[400]',
+
+seqlen = 80,
+
+prefix = '/home/lvapeab/smt/software/GroundHog/tutorials/models/ue/en_400_400'
+)

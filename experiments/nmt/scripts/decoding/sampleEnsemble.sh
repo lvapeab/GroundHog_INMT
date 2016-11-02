8073bb560f90941d2fd8fdb1d2c6d48859de09b0
Alvaro <lvapeab@gmail.com>
2016-11-02 09:45:35 +0100

    Sample ensemble

--- experiments/nmt/scripts/decoding/sampleEnsemble.sh
+++ experiments/nmt/scripts/decoding/sampleEnsemble.sh
@@ -1,52 +1,31 @@
 #!/bin/bash
-sampler=/home/lvapeab/smt/software/GroundHog/experiments/nmt/sample_ensemble.py
+sampler=${SOFTWARE_PREFIX}/GroundHog/experiments/nmt/sample_ensemble.py
 
 
-#usage: Sample (of find with beam-serch) translations from a translation model
-#       [-h] --state STATE [--beam-search] [--beam-size BEAM_SIZE]
-#       [--ignore-unk] [--source SOURCE] [--trans TRANS] [--normalize]
-#       [--verbose]
-#       model_path [changes]
-#
-#positional arguments:
-#  model_path            Path to the model
-#  changes               Changes to state
-#
-#optional arguments:
-#  -h, --help            show this help message and exit
-#  --state STATE         State to use
-#  --beam-search         Beam size, turns on beam-search
-#  --beam-size BEAM_SIZE
-#                        Beam size
-#  --ignore-unk          Ignore unknown words
-#  --source SOURCE       File of source sentences
-#  --trans TRANS         File to save translations in
-#  --normalize           Normalize log-prob with the word count
-#  --verbose             Be verbose
+beamsize=12
+src_lan="es"
+trg_lan="en"
+task="xerox"
+split="dev"
 
-task=europarl
-split=test
-src_lan="en"
-dest_lan="fr"
+v=1
 
 source_file=${DATA_PREFIX}/$task/DATA/${split}.${src_lan}
-refs=${DATA_PREFIX}/$task/DATA//${split}.${dest_lan}
+refs=${DATA_PREFIX}/$task/DATA/${split}.${trg_lan}
 
-model_name="europarl_1000_620_"
-prefix=${MODELS_PREFIX}/${task}/${src_lan}${dest_lan}/NMT/models/${model_name}
-beamsize=12
+model_infix="_289_354_"
 
-state=${prefix}state.pkl
-m1=${prefix}model_bleu98.npz
-m2=${prefix}model_bleu103.npz
-m3=${prefix}model_bleu105.npz
+state="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/models/${task}${model_infix}state.pkl"
+m1="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/models/${task}${model_infix}best_bleu_model.npz"
+m2="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/models/${task}${model_infix}model_bleu6.npz"
+m3="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/models/${task}${model_infix}model_bleu50.npz"
+m4="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/models/${task}${model_infix}model_bleu59.npz"
+m5="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/models/${task}${model_infix}model_bleu62.npz"
 
-v=""
-for beamsize in 12 20; 
-do
-    echo "Beam_size: $beamsize"
-    dest_file=${MODELS_PREFIX}/${task}/${src_lan}${dest_lan}/NMT/translations/${task}.${split}.beam_${beamsize}.hyp.${dest_lan}
-    time python ${sampler} --beam-search --beam-size ${beamsize}  --state ${state}  --source ${source_file} --trans ${dest_file} ${v} --models ${m1} ${m2} ${m3} # ${m4} ${m5}
 
-    echo "Beam: $beamsize. `thot_calc_bleu -r  ${refs} -t ${dest_file}`"
-done
+echo "Beam_size: $beamsize"
+dest="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/Online/${split}.OFFLINE.ensemble.${trg_lan}"
+
+time python ${sampler} --beam-search --beam-size ${beamsize} --notReplaceUnk --state ${state}  --source ${source_file} --trans ${dest} --models ${m1} ${m2} ${m3} ${m4} ${m5}
+
+echo "Beam: $beamsize. `calc_bleu -r  ${refs} -t ${dest}`"

89bceb79249a3d12061e27dbebb7a03d3a6d9c81
Alvaro <lvapeab@gmail.com>
2016-11-02 09:45:48 +0100

    xerox_online

--- experiments/nmt/scripts/online_training/launch_xerox_online.sh
+++ experiments/nmt/scripts/online_training/launch_xerox_online.sh
@@ -18,8 +18,10 @@ model_infix="_289_354_"
 
 state="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/models/${task}${model_infix}state.pkl"
 m1="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/models/${task}${model_infix}best_bleu_model.npz"
-
-
+m2="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/models/${task}${model_infix}model_bleu6.npz"
+m3="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/models/${task}${model_infix}model_bleu50.npz"
+m4="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/models/${task}${model_infix}model_bleu59.npz"
+m5="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/models/${task}${model_infix}model_bleu62.npz"
 #################################################
 
 
@@ -30,13 +32,13 @@ task="xerox"
 source="${DATA_PREFIX}/${task}/DATA/${split}.${src_lan}"
 refs="${DATA_PREFIX}/${task}/DATA/${split}.${trg_lan}"
 
-for algo in "AdaGrad" ; do
-    for lr in 1.0 0.5 0.01 0.005 0.001 0.0005 0.0001 0.00005 0.00001 ; do
+for algo in "SGD" ; do
+    for lr in 0.1 0.05; do # 1.0 0.5 0.1 0.05 0.01 0.005 0.001 0.0005 0.0001 0.00005 0.00001 ; do
         for n_iter in 10; do
             echo "algo: ${algo}"
             dest="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/Online/${split}.${algo}.${lr}.${trg_lan}"
             mkdir -p `dirname ${dest}`
-            python ${pe_script} --verbose ${v} --algo ${algo} --lr ${lr} --beam-search --beam-size ${beam_size} --state ${state} --source ${source} --trans ${dest} --refs ${refs} --n-iters ${n_iter} --models ${m1}
+            python ${pe_script} --verbose ${v} --algo ${algo} --lr ${lr} --beam-search --beam-size ${beam_size} --state ${state} --source ${source} --trans ${dest} --refs ${refs} --n-iters ${n_iter} --models ${m1} # ${m2} ${m3} ${m4} ${m5}
         done
     done
 done

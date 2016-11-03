362aa465258b3d079b672f41d19a6ef9d3ce6e45
lvapeab <lvapeab@gmail.com>
2016-11-03 17:35:08 +0100

    Online training dirs and split scripts

--- experiments/nmt/scripts/online_training/xerox/esen/xerox_sgd.sh
+++ experiments/nmt/scripts/online_training/xerox/esen/xerox_sgd.sh
@@ -33,7 +33,7 @@ source="${DATA_PREFIX}/${task}/DATA/${split}.${src_lan}"
 refs="${DATA_PREFIX}/${task}/DATA/${split}.${trg_lan}"
 
 for algo in "SGD" ; do
-    for lr in 0.1 0.05; do # 1.0 0.5 0.1 0.05 0.01 0.005 0.001 0.0005 0.0001 0.00005 0.00001 ; do
+    for lr in 1.0 0.5 0.1 0.05 0.01 0.005 0.001 0.0005 0.0001 0.00005 0.00001 ; do
         for n_iter in 10; do
             echo "algo: ${algo}"
             dest="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/Online/${split}.${algo}.${lr}.${trg_lan}"

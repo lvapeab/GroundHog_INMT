2ca1cb19b6c93df91571e18aaf06df917b8df18c
Alvaro <lvapeab@gmail.com>
2016-11-10 09:53:53 +0100

    PosUnk in sample ensemble script

--- experiments/nmt/scripts/decoding/sampleEnsemble.sh
+++ experiments/nmt/scripts/decoding/sampleEnsemble.sh
@@ -3,17 +3,18 @@ sampler=${SOFTWARE_PREFIX}/GroundHog/experiments/nmt/sample_ensemble.py
 
 
 beamsize=12
-src_lan="en"
-trg_lan="es"
-task="xerox"
+src_lan="es"
+trg_lan="en"
+task="ue"
 split="dev"
 
 v=1
 
 source_file=${DATA_PREFIX}/$task/DATA/${split}.${src_lan}
 refs=${DATA_PREFIX}/$task/DATA/${split}.${trg_lan}
+topn="${DATA_PREFIX}/${task}/NMT_DATA/topn_${trg_lan}.pkl"
 
-model_infix="_600_420_2211_"
+model_infix="_480_480"
 
 state="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/models/${task}${model_infix}state.pkl"
 m1="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/models/${task}${model_infix}best_bleu_model.npz"
@@ -27,6 +28,6 @@ echo "Beam_size: $beamsize"
 dest="${MODELS_PREFIX}/${task}/${src_lan}${trg_lan}/Online/${split}.OFFLINE.ensemble.${trg_lan}"
 mkdir -p `dirname $dest`
 
-time python ${sampler} --beam-search --beam-size ${beamsize} --notReplaceUnk --state ${state}  --source ${source_file} --trans ${dest} --models ${m1} # ${m2} ${m3} ${m4} ${m5}
+time python ${sampler} --beam-search --beam-size ${beamsize} --mapping ${topn} --heuristic 1 --state ${state}  --source ${source_file} --trans ${dest} --models ${m1} # ${m2} ${m3} ${m4} ${m5}
 
 echo "Beam: $beamsize. `calc_bleu -r  ${refs} -t ${dest}`"
